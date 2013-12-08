4bbecb9853639cf15e330a011808a12234895533
Melissa Holmes <meltar@gmail.com>
2013-12-07 21:49:56 -0500

    Add content to static_pages#help

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,4 +9,12 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
   end
+
+	describe "Help page" do
+
+		it "should have the content 'Help'" do
+			visit '/static_pages/help'
+			expect(page).to have_content('Help')
+		end
+	end
 end
