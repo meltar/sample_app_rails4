3f0caea864335898aa837af91b64712895f01cb4
Melissa Holmes <meltar@gmail.com>
2013-12-09 16:38:11 -0500

    Add route and initial content for users#new
    *Add specs
    *Update signup link on static_pages#home view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 SampleAppRail4::Application.routes.draw do
   get "users/new"
+
 	root	'static_pages#home'
+  match '/signup',		to: 'users#new',						via: 'get'
   match '/home',			to: 'static_pages#home',		via: 'get'
   match '/help',			to: 'static_pages#help',		via: 'get'
   match '/about',			to: 'static_pages#about',		via: 'get'
