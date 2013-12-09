c032de81cf94f16e64362e460cdcaec4c9e87c32
Melissa Holmes <meltar@gmail.com>
2013-12-09 18:14:09 -0500

    Add tests for links based on Listing 5.40

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,21 @@ describe "StaticPages" do
 
     it_should_behave_like "all static pages"
 	end
+
+	describe "layout" do
+		it "should have the right links on the layout" do
+			visit root_path
+			click_link "About"
+			expect(page).to have_title(full_title('About Us'))
+			click_link "Help"
+			expect(page).to have_title(full_title('Help'))
+			click_link "Contact"
+			expect(page).to have_title(full_title('Contact'))
+			click_link "Home"
+			click_link "Sign up now!"
+			expect(page).to have_title(full_title('Sign up'))
+			click_link "sample app"
+			expect(page).to have_title(full_title(''))
+		end
+	end
 end
