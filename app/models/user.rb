432e93f26c5d260c683d7245d965adc60de306eb
Melissa Holmes <meltar@gmail.com>
2013-12-10 14:42:51 -0500

    Ensure the user has a secure password
    *The password and password_confirmation must match

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ActiveRecord::Base
 	validates :email, presence: true,
 										format: { with: VALID_EMAIL_REGEX },
 										uniqueness: { case_sensitive: false }
+
+	has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe User do
 
 	before do
-		@user = User.new(name: "Example User", email: "user@example.com")
+		@user = User.new(name: "Example User",
+										 email: "user@example.com",
+										 password: "foobar",
+										 password_confirmation: "foobar",
+										)
 	end
 
 	subject { @user }
@@ -11,6 +15,8 @@ describe User do
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
 	context "validations" do
 		it { should be_valid }
@@ -60,6 +66,21 @@ describe User do
 				user_with_same_email.email = @user.email.upcase
 				user_with_same_email.save
 			end
+
+			it { should_not be_valid }
+		end
+
+		describe "when password is not present" do
+			before do
+				@user = User.new(name: "Example User", email: "user@example.com",
+												 password: " ", password_confirmation: " ")
+			end
+
+			it { should_not be_valid }
+		end
+
+		describe "when password doesn't match confirmation" do
+			before { @user.password_confirmation = "mismatch" }
 
 			it { should_not be_valid }
 		end
