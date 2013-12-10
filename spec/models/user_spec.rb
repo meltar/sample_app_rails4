bbefe143d7bea4c8c35a3eadbbb726d0714fd0cc
Melissa Holmes <meltar@gmail.com>
2013-12-10 14:54:12 -0500

    Add tests for authenticate method for passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,7 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:authenticate) }
 
 	context "validations" do
 		it { should be_valid }
@@ -83,6 +84,22 @@ describe User do
 			before { @user.password_confirmation = "mismatch" }
 
 			it { should_not be_valid }
+		end
+
+		describe "returns value of authenticate method" do
+			before { @user.save }
+			let(:found_user) { User.find_by(email: @user.email) }
+
+			describe "with valid password" do
+				it { should eq found_user.authenticate(@user.password) }
+			end
+
+			describe "with invalid password" do
+				let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+				it { should_not eq user_for_invalid_password }
+				specify { expect(user_for_invalid_password).to be_false }
+			end
 		end
 	end
 end
