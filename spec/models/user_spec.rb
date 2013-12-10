25652f4f5407a936e961a47b276d64ecffa7a638
Melissa Holmes <meltar@gmail.com>
2013-12-10 14:34:29 -0500

    Add password_digest column to User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,6 +10,7 @@ describe User do
 
 	it { should respond_to(:name) }
 	it { should respond_to(:email) }
+	it { should respond_to(:password_digest) }
 
 	context "validations" do
 		it { should be_valid }
