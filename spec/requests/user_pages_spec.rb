8dee841c4da005262662e975f88adfb70c876024
Melissa Holmes <meltar@gmail.com>
2013-12-12 20:43:59 -0500

    Update tests to match tutorial

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -42,9 +42,10 @@ describe "UserPages" do
 
 			describe "after submission" do
 				before { click_button submit }
+				let(:user) { User.find_by(email: 'user@example.com') }
 
-				it  { should have_title("Example User") }
-				it  { should have_content("Welcome") }
+				it  { should have_title(user.name) }
+				it  { should have_selector('div.alert.alert-success', text: 'Welcome') }
 			end
 		end
 	end
