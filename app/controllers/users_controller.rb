debc8fe3dc0b3248d9b19fce321edf804acd5521
Melissa Holmes <meltar@gmail.com>
2014-01-02 19:02:10 -0500

    Only logged in users can edit the user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :signed_in_user, only: [:edit, :update]
+
   def new
 		@user = User.new
   end
@@ -40,5 +42,11 @@ class UsersController < ApplicationController
 																 :password,
 																 :password_confirmation,
 																)
+	end
+
+	# Before filters
+
+	def signed_in_user
+		redirect_to signin_url, notice: "Please sign in." unless signed_in?
 	end
 end
