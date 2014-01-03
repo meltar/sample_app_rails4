1f3ebd04f3c8abbfbb9d74068201fca1d480e917
Melissa Holmes <meltar@gmail.com>
2014-01-03 17:30:25 -0500

    Add admin column to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,9 +19,12 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:admin) }
+
 
 	context "validations" do
 		it { should be_valid }
+		it { should_not be_admin }
 
 		describe "when name is not present" do
 			before { @user.name = " " }
@@ -122,6 +125,15 @@ describe User do
 		describe "remember token" do
 			before { @user.save }
 			its(:remember_token) { should_not be_blank }
+		end
+
+		describe "with admin attribute set to 'true'" do
+			before do
+				@user.save!
+				@user.toggle!(:admin)
+			end
+
+			it { should be_admin }
 		end
 	end
 end
