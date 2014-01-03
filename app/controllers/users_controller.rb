b6acafcfda3ef36800ffe8aeba60105c882ae757
Melissa Holmes <meltar@gmail.com>
2014-01-02 20:25:58 -0500

    Only allow the user to edit their own settings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-	before_action :signed_in_user, only: [:edit, :update]
+	before_action :signed_in_user,	only: [:edit, :update]
+	before_action :correct_user,		only: [:edit, :update]
 
   def new
 		@user = User.new
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
 		if @user.update_attributes(user_params)
 			flash[:success] = "Profile updated"
 			redirect_to @user
@@ -48,5 +47,10 @@ class UsersController < ApplicationController
 
 	def signed_in_user
 		redirect_to signin_url, notice: "Please sign in." unless signed_in?
+	end
+
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
 	end
 end
