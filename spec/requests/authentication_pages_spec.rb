fc22c3d0b6902420f4760b1a1ddc09b1472aa9da
Melissa Holmes <meltar@gmail.com>
2014-01-13 14:24:19 -0500

    Add tests to check view when signed out
    Certain links should not be visible without signing in first.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -50,6 +50,16 @@ describe "Authentication" do
 		describe "for non-signed in users" do
 			let(:user) { FactoryGirl.create(:user) }
 
+			describe "when not signed in" do
+				before { visit root_path }
+
+				it { should_not have_link('Users',				href: users_path) }
+				it { should_not have_link('Profile',			href: user_path(user)) }
+				it { should_not have_link('Settings',			href: edit_user_path(user)) }
+				it { should_not have_link('Sign out',			href: signout_path) }
+				it { should have_link('Sign in',	href: signin_path) }
+			end
+
 			describe "when attempting to visit a protected page" do
 				before do
 					visit edit_user_path(user)
