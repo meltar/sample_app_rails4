56c92f77a2b9d83d1db9cae85498d4aac4dc11f4
Melissa Holmes <meltar@gmail.com>
2014-01-14 14:38:53 -0500

    Make sure the posts are ordered from newest to oldest

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -135,5 +135,20 @@ describe User do
 
 			it { should be_admin }
 		end
+
+		describe "micropost associations" do
+
+			before { @user.save }
+			let!(:older_micropost) do
+				FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+			end
+			let!(:newer_micropost) do
+				FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+			end
+
+			it "should have the right microposts in the right order" do
+				expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+			end
+		end
 	end
 end
