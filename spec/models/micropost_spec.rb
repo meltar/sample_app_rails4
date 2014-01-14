dd3fc1507d74e553fad971e0e529a51ce151a709
Melissa Holmes <meltar@gmail.com>
2014-01-14 13:33:56 -0500

    Add validation - micropost must have a user_id

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -12,4 +12,9 @@ describe Micropost do
 
 	it { should respond_to(:content) }
 	it { should respond_to(:user_id) }
+
+	describe "when the user_id is not present" do
+		before { @micropost.user_id = nil }
+		it { should_not be_valid }
+	end
 end
