11e36e7231af740c61da2e1f213779d9910b83a2
Melissa Holmes <meltar@gmail.com>
2014-01-15 19:18:47 -0500

    Display the signed in user's feed on the home page

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,7 @@ describe User do
 	it { should respond_to(:authenticate) }
 	it { should respond_to(:admin) }
 	it { should respond_to(:microposts) }
+	it { should respond_to(:feed) }
 
 	context "validations" do
 		it { should be_valid }
@@ -157,6 +158,16 @@ describe User do
 				microposts.each do |micropost|
 					expect(Micropost.where(id: micropost.id)).to be_empty
 				end
+			end
+
+			describe "status" do
+				let(:unfollowed_post) do
+					FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+				end
+
+				its(:feed) { should include(newer_micropost) }
+				its(:feed) { should include(older_micropost) }
+				its(:feed) { should_not include(unfollowed_post) }
 			end
 		end
 	end
