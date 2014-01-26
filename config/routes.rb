3257785874ae82439d804126eb8d23f141387454
Melissa Holmes <meltar@gmail.com>
2014-01-26 17:40:06 -0500

    Create routes and partials to follow and unfollow users

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ SampleAppRail4::Application.routes.draw do
 			get :following, :followers
 		end
 	end
-	resources :sessions, only: [:new, :create, :destroy]
-	resources :microposts, only: [:create, :destroy]
+	resources :sessions,			only: [:new, :create, :destroy]
+	resources :microposts,		only: [:create, :destroy]
+	resources :relationships, only: [:create, :destroy]
 	root	'static_pages#home'
   match '/signup',		to: 'users#new',						via: 'get'
   match '/signin',		to: 'sessions#new',					via: 'get'
