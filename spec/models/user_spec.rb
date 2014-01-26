b6ec778b76b5b60522f9e074ef07bbc172d81e6a
Melissa Holmes <meltar@gmail.com>
2014-01-26 16:34:06 -0500

    Add helpers for follow! and following? users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,6 +24,8 @@ describe User do
 	it { should respond_to(:feed) }
 	it { should respond_to(:relationships) }
 	it { should respond_to(:followed_users) }
+	it { should respond_to(:following?) }
+	it { should respond_to(:follow!) }
 
 	context "validations" do
 		it { should be_valid }
@@ -172,5 +174,16 @@ describe User do
 				its(:feed) { should_not include(unfollowed_post) }
 			end
 		end
+	end
+
+	describe "following" do
+		let(:other_user) { FactoryGirl.create(:user) }
+		before do
+			@user.save
+			@user.follow!(other_user)
+		end
+
+		it { should be_following(other_user) }
+		its(:followed_users) { should include(other_user) }
 	end
 end
